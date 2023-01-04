01e208ee06edfa28138cd15673a79c4c37250fb8
prasertcbs <bluecotton.k@gmail.com>
2023-01-04 11:15:20 +0700

    add matplotlib example

---
+++ flower_art.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set up the figure and subplot
+fig, ax = plt.subplots()
+
+# Set the number of petals and the radius of the circles
+num_petals = 6
+circle_radius = 0.5
+
+# Create the petals as circles
+for i in range(num_petals):
+    angle = i * 2 * np.pi / num_petals
+    x = circle_radius * np.cos(angle)
+    y = circle_radius * np.sin(angle)
+    ax.add_patch(plt.Circle((x, y), radius=0.5, color='r'))
+
+# Create the center of the flower as a yellow circle
+ax.add_patch(plt.Circle((0, 0), radius=0.2, color='y'))
+
+# Display the plot
+plt.axis('equal')
+plt.show()
---
+++ heart_shape.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set up the figure and subplot
+fig, ax = plt.subplots()
+
+# Create a list of points for the curve of the heart
+t = np.linspace(0, 2 * np.pi, 100)
+x = 16 * np.sin(t) ** 3
+y = 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
+points = np.array([x, y]).T
+
+# Plot the curve of the heart
+plt.plot(x, y, color='r', linewidth=2)
+
+# Fill in the heart with the color red
+plt.fill(x, y, color='r', alpha=0.5)
+
+# Display the plot
+plt.axis('equal')
+plt.show()
